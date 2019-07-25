b4a81a61481c4e2f1a41dd14a82865d5c3cc37a1
mayukhnandy <mayukhnandy13@gmail.com>
2019-07-25 01:44:31 -0700

    42

--- JavaDemo/src/User.java
+++ JavaDemo/src/User.java
@@ -1,7 +1,7 @@
 
 public class User {
 	public User() {
-		// TODO Auto-generated constructor stub
+		// Auto-generated constructor stub
 	}
 	public static void main(String[] args) {
 		//  Auto-generated method stub
