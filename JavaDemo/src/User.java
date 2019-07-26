9ff82e7922bac3b4f15e3ffea41fe74762abfa37
mayukhnandy <mayukhnandy13@gmail.com>
2019-07-26 04:42:25 -0700

    42

--- JavaDemo/src/User.java
+++ JavaDemo/src/User.java
@@ -1,10 +1,42 @@
+import java.util.ArrayList;
 
-public class User {
-	public User() {
-		//Auto-generated constructor stub
+public class User<wishlist> {
+	private String name; 
+	private ArrayList<Item> wishlist;
+	private User mytaste;
+	private User con;
+	
+	public User(String name, User con){
+		this.name = name;
+                this.con = con;
+		wishlist = new ArrayList<Item>();
+	}
+	//42
+	public String getName(){
+		return name;
+	}
+
+	public ArrayList<Item> getWishlist(){
+		return wishlist;
+	}
+	
+	public void addToWishlist(Item item){
+		wishlist.add(item);
+	}
+	
+	public void removeFromWishlist(Item item){
+		wishlist.remove(item);
+	}
+      
+	public User getTasteManager(){
+		return mytaste;
+	}
+
+        public Item getItemFromWishlist(int i){
+		return wishlist.get(i);
 	}
 	public static void main(String[] args) {
-		//  Auto-generated method stub
+		//Auto-generated method stub
 
 	}
 
